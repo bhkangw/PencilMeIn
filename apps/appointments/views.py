d2df312313e1c3e7c495b2b3b809e7f00760fce7
Brian Kang <bhkangw@gmail.com>
2017-11-17 14:32:27 -0800

    Updating with submitted exam files

--- apps/appointments/views.py
+++ apps/appointments/views.py
@@ -11,7 +11,11 @@ def index(request):
         return redirect('/')
     today = datetime.now().strftime("%Y-%m-%d")
     user = User.objects.get(id=request.session['user_id'])
-    context = {'app': Appointment.objects.order_by('time').filter(user=user)}
+    context = {
+        # 'app': Appointment.objects.order_by('time').filter(user=user),
+        'today': Appointment.objects.filter(date=today, user=user),
+        'later': Appointment.objects.filter(user=user).exclude(date=today)
+    }
     return render(request, 'appointments/index.html', context, today)
 
 
@@ -29,7 +33,7 @@ def update(request, id):
         print "!!!!!"
         app = Appointment.objects.get(id=id)
         app.task = request.POST['task']
-        # app.date = request.POST['date']
+        app.date = request.POST['date']
         app.time = request.POST['time']
         app.status = request.POST['status']
         app.save()
